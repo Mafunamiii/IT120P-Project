2d020cb70abd700325e81aa130f1de548f899f85
SirLalaPyon <76900263+SirLalaPyon@users.noreply.github.com>
2023-04-01 14:55:25 +0800

    Added Register Page

---
+++ FinanceKo/app/src/main/java/com/example/financeko/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.financeko;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_registration);
+    }
+}
